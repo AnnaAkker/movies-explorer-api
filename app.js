e3342aafd20b91168a2c483cf2098ecea1565838
AnnaAkker <AnnaBorSi@yandex.ru>
2023-11-12 15:57:00 +0300

    4 stage

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { limiter } = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/handlerErr');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
